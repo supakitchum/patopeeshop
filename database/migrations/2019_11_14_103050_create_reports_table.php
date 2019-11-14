509b6abf864c1025053c00cce590cf01a84d3553
supakit chumchanchira <supakit_c@cmu.ac.th>
2019-11-14 17:51:44 +0700

    migrate db.

---
+++ database/migrations/2019_11_14_103050_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('name')->nullable();
+            $table->integer('order_id');
+            $table->string('detail')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
