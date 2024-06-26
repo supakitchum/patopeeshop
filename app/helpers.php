ebcccb19c18c8353761cb2352f60557fe4bda744
supakit <supakit_c@cmu.ac.th>
2024-06-26 22:08:11 +0700

    update title

--- app/helpers.php
+++ app/helpers.php
@@ -3,11 +3,10 @@ if (!function_exists('get_title')) {
     function get_title()
     {
         $title = View::getSection('title');
-        if ($title){
-            return $title." : ประตูผี";
-        }
-        else{
-            return "INETCV";
+        if ($title) {
+            return $title . " : " . env('APP_NAME');
+        } else {
+            return env('APP_NAME');
         }
     }
 }
